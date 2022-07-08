c20e8c4a6a6f5215f94163e9ed3a44c369f35483
RayGong1 <104469223+RayGong1@users.noreply.github.com>
2022-07-08 12:22:46 -0400

    Update oridir_generate_mock_docs.m

--- +ndi/+calc/+vis/+test/oridir_generate_mock_docs.m
+++ +ndi/+calc/+vis/+test/oridir_generate_mock_docs.m
@@ -8,20 +8,20 @@ function docs = oridir_generate_mock_docs(type, number)
 switch type
     case standard
         for x = 1.0:+1.0:number
-            P(x,:)= [x mod(x*10.0,40) 0.5*mod(x*10.0,40) 90-mod(x,2)*45 x*30]; %This is the standard preset value
+            P(x,:)= [x mod(x*10.0,40) 0.5*mod(x*10.0,40) 90-mod(x,2)*45 x*30]; % This is the standard preset value
         end
    
     case random_noisy
         for x = 1.0:+1.0:number
-            P(x,:)= ndi.calc.vis.test.oridir_mock_parameters; %This is the parameters with noise, I used the model 10% of response + 2
-            P(x,1) = P(x,1)*1.1 + 2;
+            P(x,:)= ndi.calc.vis.test.oridir_mock_parameters; % This is the parameters with noise, I used the model 10% of response + 2. I'm not quite sure is this the 
+            P(x,1) = P(x,1)*1.1 + 2;                            % right way to call a function in MATLAB
             P(x,2) = P(x,2)*1.1 + 2;
             P(x,3) = P(x,3)*1.1 + 2;
         end
 
     case random_nonoise
         for x = 1.0:+1.0:number
-            P(x,:)= ndi.calc.vis.test.oridir_mock_parameters; %This is parameters without noise pulled straight from the function ndi.calc.vis.test.oridir_mock_parameters
+            P(x,:)= ndi.calc.vis.test.oridir_mock_parameters; % This is parameters without noise pulled straight from the function ndi.calc.vis.test.oridir_mock_parameters
         end
 
 end
