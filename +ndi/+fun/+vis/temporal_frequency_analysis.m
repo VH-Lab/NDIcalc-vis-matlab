cc4ce7ea750e59cf0f3d2db8d2b5a0f8d3487f34
Steve Van Hooser <vanhoosr@brandeis.edu>
2023-05-12 08:19:56 -0400

    initial work on temporal frequency

--- +ndi/+fun/+vis/temporal_frequency_analysis.m
+++ +ndi/+fun/+vis/temporal_frequency_analysis.m
@@ -1,4 +1,4 @@
-function [assoc]=tfanalysis_compute(resp)
+function [tf_props]=temporal_frequency_analysis(resp)
 
 %  TFANALYSIS_COMPUTE  Analyze reponses to temporal frequencies
 % %  [ASSOC]=TFANALYSIS_COMPUTE(RESP)
@@ -33,12 +33,6 @@ function [assoc]=tfanalysis_compute(resp)
 %  'TF Low TF'                |   low TF with half of max response 
 %  'TF High TF'               |   high TF with half of max response
 %
-%  'TF sig resp p'            |   p value of ANOVA across conditions
-%  'TF sig resp'              |   (0/1) Is above < 0.05?
-%  'TF visual response p'     |   p value of ANOVA across conditions + 
-%                             |     blank, if available
-%  'TF visual response'       |   (0/1) Is above < 0.05?
-%
 %  Difference of gaussians fit:
 %  'TF DOG params'            |   'r0 re se ri si'
 %  'TF DOG Fit'               |   1st row has TF values, 2nd has responses
@@ -54,33 +48,17 @@ function [assoc]=tfanalysis_compute(resp)
 %  'TF spline High'           |   High cut-off, as measured with spline
 %
 
-assoclist = { 'TF Low TF','TF High TF',...
-	'TF Response curve','TF Pref','TF Low','TF High',...
-	'TF sig resp p','TF sig resp','TF visual response p','TF visual response','TF Blank Response',...
-	'TF DOG params','TF DOG Fit','TF DOG R2','TF DOG Low','TF DOG High','TF DOG Pref',...
-	'TF spline Fit','TF spline Pref','TF spline Low','TF spline High'};
 
-assoc=struct('type','t','owner','t','data',0,'desc',0); assoc=assoc([]);
+  % STEP 1: empirical parameters (fitless)
 
-if nargin==0, assoc = assoclist; return; end;
-
-r0=resp.spont(1);
-if isfield(resp,'blankresp'),
-	r0 = resp.blankresp(1);
-end;
-
-% from the raw curve
 [mf,preftf]=max(resp.curve(2,:));
 preftf = resp.curve(1,preftf(1));
 [lowv, maxv, highv] = compute_halfwidth(resp.curve(1,:),resp.curve(2,:));
-[lowvsp, maxvsp, highvsp] = compute_halfwidth(resp.curve(1,:),resp.curve(2,:)-r0);
 
- % significance
- 
-[tfsigp,tfvp] = neural_response_significance(resp);
+[tf_props.fitless.L50,tf_props.fitless.Pref,tf_props.fitless.H50] = ...
+	ndi.fun.vis.compute_halfwidth(resp.curve(1,:),resp.curve(2,:));
 
- % DOG fit
-re = mf; ri = mf; se = maxv; si = 5+5*randn;
+ % STEP 2: DOG fit
 rcurve = resp.curve;
 
 search_options=optimset('fminsearch');
@@ -93,12 +71,12 @@ norm_error_overall = Inf;
 dog_par_overall = [];
 
 for jj=1:10,
-	re = mf; ri = mf; se = maxv; si = 5+5*randn;
+	r0 = 0; re = mf; ri = mf; se = maxv; si = 5+5*randn;
 	dog_par=fminsearch('dog_error',[r0 re se ri si],search_options,...
-		[rcurve(1,:) 60 70],[rcurve(2,:) r0 r0], ...
+		[rcurve(1,:) 100 120],[rcurve(2,:) 0 0], ...
 		[rcurve(4,:) mean(rcurve(4,:)) mean(rcurve(4,:))] 	    )';
 
-	norm_error=dog_error(dog_par, [rcurve(1,:) 60 70],[rcurve(2,:) r0 r0]);
+	norm_error=dog_error(dog_par, [rcurve(1,:) 100 120],[rcurve(2,:) 0 0 ]);
 
 	if norm_error<norm_error_overall,
 		dog_par_overall = dog_par;
@@ -120,39 +98,30 @@ else,
 end;
 
 	
-[lowdog, prefdog, highdog] =   compute_halfwidth(tfrange_interp,response);
+[lowdog, prefdog, highdog] = ndi.fun.vis.compute_halfwidth(tfrange_interp,response);
+
+fit_dog.parameters = dog_par;
+fit_dog.values = tfrange_interp;
+fit_dog.fit = response;
+fit_dog.L50 = lowdog;
+fit_dog.Pref = prefdog;
+fit_dog.H50 = highdog;
+
+tf_props.fit_dog = fit_dog;
+
+ % STEP 3: spline fitting
 
 fitx = min(rcurve(1,:)):1:max(rcurve(1,:));
 if fitx(end)~=max(rcurve(1,:)), fitx(end+1) = max(rcurve(1,:)); end;
 fity = interp1([rcurve(1,:)],[rcurve(2,:)], fitx,'spline');
-[lowspline, prefspline, highspline] = compute_halfwidth(fitx,fity);
+[lowspline, prefspline, highspline] = ndi.fun.vis.compute_halfwidth(fitx,fity);
 
-assoc(end+1) = tfassoc('TF Response curve',rcurve,'TF Response curve');
-if isfield(resp,'blankresp'),
-        assoc(end+1)=tfassoc('TF Blank Response',resp.blankresp,'TF blank resp');
-end;
-assoc(end+1) = tfassoc('TF Pref',preftf,'TF Pref');
-assoc(end+1) = tfassoc('TF Low',lowv,'TF Low cut-off (half-max)');
-assoc(end+1) = tfassoc('TF High',highv,'TF High cut-off (half-max)');
+fit_spline.values = tfrange_interp;
+fit_spline.fit = response;
+fit_spline.L50 = lowspline;
+fit_spline.Pref = prefspline;
+fit_spline.H50 = highspline;
 
-assoc(end+1) = tfassoc('TF Low TF',lowvsp,'TF Low cut-off (half-max) TF');
-assoc(end+1) = tfassoc('TF High TF',highvsp,'TF High cut-off (half-max) TF');
+tf_props.fit_spline = fit_spline;
 
-assoc(end+1) = tfassoc('TF sig resp p',tfsigp,'TF response p value');
-assoc(end+1) = tfassoc('TF sig resp',tfsigp<=0.05,'Is TF response significant?');
-assoc(end+1) = tfassoc('TF visual response p',tfvp,'TF visual response (including blank) p value');
-assoc(end+1) = tfassoc('TF visual response',tfvp<=0.05,'Is TF response across stims and blank significant?');
 
-assoc(end+1) = tfassoc('TF DOG Pref',prefdog,'TF Pref difference of gaussians');
-assoc(end+1) = tfassoc('TF DOG Low',lowdog,'TF Low cut-off (half-max from DOG)');
-assoc(end+1) = tfassoc('TF DOG High',highdog,'TF High cut-off (half-max from DOG)');
-assoc(end+1) = tfassoc('TF DOG R2',r2,'TF DOG r^2 of fit');
-assoc(end+1) = tfassoc('TF DOG Fit',[tfrange_interp; response],'TF DOG Fit, 1st row is TF, 2nd row is response');
-
-assoc(end+1) = tfassoc('TF spline Pref',prefspline,'TF Pref spline');
-assoc(end+1) = tfassoc('TF spline Low',lowspline,'TF Low cut-off (half-max from spline)');
-assoc(end+1) = tfassoc('TF spline High',highspline,'TF High cut-off (half-max from spline)');
-assoc(end+1) = tfassoc('TF spline Fit',[fitx; fity],'TF spline Fit, 1st row is TF, 2nd row is response');
-
-function myassoc = tfassoc(type,data,desc)
-myassoc  = struct('type',type,'owner','tf','data',data,'desc',desc);
